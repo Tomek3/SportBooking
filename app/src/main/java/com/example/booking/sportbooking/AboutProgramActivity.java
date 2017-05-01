1ca3b73408b272415ef2049bcef77fc7afd825fb
Tomek3 <tomaszfielek@tlen.pl>
2017-05-01 13:01:41 +0200

    about program

--- app/src/main/java/com/example/booking/sportbooking/AboutProgramActivity.java
+++ app/src/main/java/com/example/booking/sportbooking/AboutProgramActivity.java
@@ -2,6 +2,10 @@ package com.example.booking.sportbooking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class AboutProgramActivity extends BaseActivity {
 
@@ -12,6 +16,17 @@ public class AboutProgramActivity extends BaseActivity {
         navigationView.getMenu().getItem(0).setChecked(false);
         navigationView.getMenu().getItem(3).getSubMenu().getItem(0).setChecked(true);
         setTitle(R.string.nav_item_about_program);
+
+        final TextView mTextView = new TextView(this);
+        mTextView.setLayoutParams(new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
+        mTextView.setGravity(Gravity.CENTER);
+        mTextView.setSingleLine(false);
+        mTextView.setText("SportBooking\nAutor: Tomasz Fielek 2017");
+
+
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.flContent);
+        frameLayout.addView(mTextView);
+
     }
 
     public void onBackPressed() {
